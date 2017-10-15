e32d0d753295136afb0ec7daf4d3c7f65697248f
yudong-94 <yudong@usc.edu>
2017-10-14 21:34:18 -0700

    impact encoding

--- Data Processing.R
+++ Data Processing.R
@@ -10,16 +10,15 @@ train = train[, c(1, 60, 2:59)]
     
 train_test = rbind(train, test)
 
+categoricals = c("ps_ind_02_cat", "ps_ind_04_cat", "ps_ind_05_cat", 
+                 "ps_car_01_cat", "ps_car_02_cat", "ps_car_03_cat",
+                 "ps_car_04_cat", "ps_car_05_cat", "ps_car_07_cat",
+                 "ps_car_06_cat", "ps_car_08_cat", "ps_car_09_cat",
+                 "ps_car_10_cat")
+
 ###########################################
 # One-hot-Coding
 
-library(ade4)
-library(data.table)
-categoricals = c("ps_ind_02_cat", "ps_ind_04_cat", "ps_ind_05_cat", 
-              "ps_car_01_cat", "ps_car_02_cat", "ps_car_03_cat",
-              "ps_car_04_cat", "ps_car_05_cat", "ps_car_07_cat",
-              "ps_car_06_cat", "ps_car_08_cat", "ps_car_09_cat",
-              "ps_car_10_cat")
 train_test$ps_ind_02_cat = as.factor(train_test$ps_ind_02_cat)
 train_test$ps_ind_04_cat = as.factor(train_test$ps_ind_04_cat)
 train_test$ps_ind_05_cat = as.factor(train_test$ps_ind_05_cat)
@@ -33,6 +32,9 @@ train_test$ps_car_07_cat = as.factor(train_test$ps_car_07_cat)
 train_test$ps_car_08_cat = as.factor(train_test$ps_car_08_cat)
 train_test$ps_car_09_cat = as.factor(train_test$ps_car_09_cat)
 train_test$ps_car_10_cat = as.factor(train_test$ps_car_10_cat)
+
+library(ade4)
+library(data.table)
 
 for (f in categoricals){
     dummy = acm.disjonctif(train_test[f])
@@ -51,4 +53,17 @@ colnames(train_test)
 
 for (f in categoricals){
     
+    newname = paste0(f,"new")
+    train_test[newname] = NA
+    
+    for (i in unique(train_test[f][,1])) {
+        perc = sum(train[train[f] == i, ]$target) / nrow(train[train[f] == i, ])
+        train_test[train_test[f] == i, newname] = perc
+    }
+    
+    train_test[f] = NULL
 }
+
+summary(train_test[,48:60])
+
+
